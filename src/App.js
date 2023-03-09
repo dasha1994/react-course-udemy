3b8ee9e4ebb1eec8ff4698d745d0a73b1ed24cc4
dasha1994 <kozy.dasha@gmail.com>
2023-03-09 18:08:57 +0200

    Transition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -11,7 +12,7 @@ class App extends Component {
   };
 
   showModal = () => {
-    this.setState({ modalIsOpen: true });
+    this.setState({ modalIsOpen: true, showBlock: false });
   };
 
   closeModal = () => {
@@ -21,8 +22,46 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.state.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exited" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => <Modal show={state} closed={this.closeModal} />}
+        </Transition>
+        <Transition in={this.state.modalIsOpen}>
+          <Backdrop show={this.state.modalIsOpen} />
+        </Transition>
+
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
