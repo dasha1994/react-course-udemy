c2006eb502919d4114dc65cf2592159f32b1ab9d
dasha1994 <kozy.dasha@gmail.com>
2023-03-13 11:42:19 +0200

    section 25

---
+++ src/components/Products/ProductItem.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { useStore } from "../../hooks-store/store";
+import Card from "../UI/Card";
+import "./ProductItem.css";
+
+const ProductItem = (props) => {
+  const dispatch = useStore()[1];
+
+  const toggleFavHandler = () => {
+    // toggleFav(props.id);
+    console.log("FAV");
+    console.log(props.id);
+    console.log(dispatch);
+
+    dispatch("TOGGLE_FAV", props.id);
+  };
+
+  return (
+    <Card style={{ marginBottom: "1rem" }}>
+      <div className="product-item">
+        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
+        <p>{props.description}</p>
+        <button
+          className={!props.isFav ? "button-outline" : ""}
+          onClick={toggleFavHandler}
+        >
+          {props.isFav ? "Un-Favorite" : "Favorite"}
+        </button>
+      </div>
+    </Card>
+  );
+};
+
+export default ProductItem;
